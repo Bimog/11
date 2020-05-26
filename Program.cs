f4e1da4c08054d9dfda88cc3e379780b550852ba
Bimog <63291149+Bimog@users.noreply.github.com>
2020-05-26 10:08:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+     public class Person
+    {
+        public string SecondName { get; set; }
+        public string Name { get; set; }
+
+        public Person(string secondName, string name)
+        {
+             
+
+                SecondName = secondName;
+                        Name=name;
+        }
+    }
+}
