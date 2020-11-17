846d9ed2c2402c081f6297701602960324873e37
Bimog <63291149+Bimog@users.noreply.github.com>
2020-11-17 11:45:18 +0300

    матрица

--- Program.cs
+++ Program.cs
@@ -1,18 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleApp4
+namespace ConsoleApp2
 {
-     public class Person
+    class Program
     {
-        public string SecondName { get; set; }
-        public string Name { get; set; }
-
-        public Person(string secondName, string name)
+        static void Main(string[] args)
         {
-             
 
-                SecondName = secondName;
-                        Name=name;
+            int[,] mas = new int[2, 2];
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    mas[i, j] = rnd.Next(0, 9);
+                }
+            }                    
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    Console.Write(mas[i, j] + "");
+                }
+                Console.WriteLine();
+            }       
+            int n = 0; 
+            bool indikator;
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                indikator = true;
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    if (mas[i, j] == 0)
+                    {
+                        indikator = false;
+                    }
+                }
+                if (indikator) { n++; }
+            }
+
+
+            Console.WriteLine("количество ненулевых строк = " + n);
+
         }
+        private static Random rnd = new Random();
     }
 }
